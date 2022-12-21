26fd3a40b111742ebd05e7e7ec66539c07a21d34
fish895623 <dan990429@gmail.com>
2022-12-21 18:02:54 +0900

    Merge Conflicts

--- main.ts
+++ main.ts
@@ -37,8 +37,8 @@ export default class MyPlugin extends Plugin {
 
   async onload() {
     await this.loadSettings()
-    await this.getOperatingSystem()
-    await this.rmButtons()
+    this.getOperatingSystem()
+    this.rmButtons()
 
     // This creates an icon in the left ribbon.
     const ribbonIconEl = this.addRibbonIcon(
@@ -48,14 +48,11 @@ export default class MyPlugin extends Plugin {
         new Notice(this.Os)
       }
     )
-    // Perform additional things with the ribbon
     ribbonIconEl.addClass('my-plugin-ribbon-class')
 
-    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
     const statusBarItemEl = this.addStatusBarItem()
     statusBarItemEl.setText('Status 이거 맞냐 Text')
 
-    // This adds a simple command that can be triggered anywhere
     this.addCommand({
       id: 'open-sample-modal-simple',
       name: 'Open sample modal (simple)',
@@ -63,7 +60,6 @@ export default class MyPlugin extends Plugin {
         new SampleModal(this.app).open()
       },
     })
-    // This adds an editor command that can perform some operation on the current editor instance
     this.addCommand({
       id: 'sample-editor-command',
       name: 'Sample editor command',
@@ -72,7 +68,6 @@ export default class MyPlugin extends Plugin {
         editor.replaceSelection('Sample Editor Command')
       },
     })
-    // This adds a complex command that can check whether the current state of the app allows execution of the command
     this.addCommand({
       id: 'open-sample-modal-complex',
       name: 'Open sample modal (complex)',
@@ -81,13 +76,10 @@ export default class MyPlugin extends Plugin {
         const markdownView =
           this.app.workspace.getActiveViewOfType(MarkdownView)
         if (markdownView) {
-          // If checking is true, we're simply "checking" if the command can be run.
-          // If checking is false, then we want to actually perform the operation.
           if (!checking) {
             new SampleModal(this.app).open()
           }
 
-          // This command will only show up in Command Palette when the check function returns true
           return true
         }
       },
